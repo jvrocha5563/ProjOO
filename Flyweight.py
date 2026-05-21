25814031e03651e000c97a036ae63e07b38b0d1e
JV-ROCHA <joao.rocha5563@gmail.com>
2026-05-21 20:24:45 -0300

    classe EspecieArvore criada

--- Flyweight.py
+++ Flyweight.py
@@ -1,1 +1,12 @@
+class EspecieArvore:
+    # __slots__ otimiza a memória
+    __slots__ = ['nome_especie', 'textura_dados', 'proj_sombra_padrao']
 
+    def __init__(self, nome_especie: str, textura_dados: str, proj_sombra_padrao: float):
+        self.nome_especie = nome_especie
+        self.textura_dados = textura_dados  # Simula uma string longa/pesada de bytes
+        self.proj_sombra_padrao = proj_sombra_padrao
+
+    def exibir(self, x: int, y: int, altura: float, largura_tronco: float):
+        # Simulação de renderização 2D
+        pass
