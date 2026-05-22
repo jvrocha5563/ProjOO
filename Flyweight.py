0056a90da70b393b8e1dc0f29b3fc330a6008e51
JV-ROCHA <joao.rocha5563@gmail.com>
2026-05-22 14:34:52 -0300

    classe Arvore criada com uso de slots para economia de memória

--- Flyweight.py
+++ Flyweight.py
@@ -10,3 +10,30 @@ class EspecieArvore:
     def exibir(self, x: int, y: int, altura: float, largura_tronco: float):
         # Simulação de renderização 2D
         pass
+
+class FabricaEspecie:
+    _especies = {}
+
+    @classmethod
+    def get_especie(cls, nome: str, textura: str, sombra: float) -> EspecieArvore:
+        if nome not in cls._especies:
+            cls._especies[nome] = EspecieArvore(nome, textura, sombra)
+        return cls._especies[nome]
+
+    @classmethod
+    def total_especies_criadas(cls) -> int:
+        return len(cls._especies)
+    
+class Arvore:
+    # Reduz drasticamente o tamanho de cada instância da árvore na memória do Python
+    __slots__ = ['x', 'y', 'altura', 'largura_tronco', 'especie']
+
+    def __init__(self, x: int, y: int, altura: float, largura_tronco: float, especie: EspecieArvore):
+        self.x = x                          # Extrínseco
+        self.y = y                          # Extrínseco
+        self.altura = altura                # Extrínseco
+        self.largura_tronco = largura_tronco # Extrínseco
+        self.especie = especie              # Referência ao Flyweight (Intrínseco)
+
+    def desenhar(self):
+        self.especie.exibir(self.x, self.y, self.altura, self.largura_tronco)
